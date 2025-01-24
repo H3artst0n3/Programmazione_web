e37f92dc970dc5a448e1ae4a3dafd2dede14f660
Angelica Rota <angirota@gmail.com>
2025-01-24 17:56:20 +0100

    API implementata, mancano alcune funzioni principali

--- app/users.js
+++ app/users.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 const db = require("./db.js");
 
 const verifyToken = (req, res, next) => {
+  console.log("cookies nella richiesta: ", req.cookies)
   const token = req.cookies["token"];
   if (!token){
+    console.log("Token non presente");
     res.status(403).send("Autenticazione fallita")
     return;
   }
@@ -12,8 +15,10 @@ const verifyToken = (req, res, next) => {
   try{
     const decoded = jwt.verify(token, "ssshhh");
     req.userId = decoded.id;
+    console.log("Token verificato, req.userId: ", req.userId);
     next();
   } catch(error) {
+    console.log("errore nella verifica", error);
     res.status(401).send("Non autorizzato!");
   }
 };
@@ -49,19 +54,19 @@ router.get('/users/', async (req, res) => {
 
 router.get("/whoami", verifyToken, async (req, res) => {
   try {
-    // const mongo = await db.connect2db();
-    console.log(req.userId)
-    // const user = await mongo.collection("users").findOne({ id: req.userId });
-    
-    if (!req.userId){
-      res.status(404).json({ msg: "Utente non loggato" });
-    }
-
-    // if (user) {
-    //   res.json(user);
-    // } else {
-    //   res.status(404).json({ msg: "Utente non trovato" });
+    const mongo = await db.connect2db();
+    const query = {id: req.userId}
+    const user = await mongo.collection("users").findOne(query);
+    console.log(user)
+    // if (!req.userId){
+    //   res.status(404).json({ msg: "Utente non loggato" });
     // }
+
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ msg: "Utente non trovato" });
+    }
   } catch (error) {
     console.error("Errore:", error);
     res.status(500).json({ message: "Errore interno del server" });
